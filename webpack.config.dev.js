1921fdd950516df21069475cf43a819a24f3d7f1
eb <elad705@gmail.com>
2021-11-06 18:01:06 +0200

    Stable build of work environment
    Includes wepack, refresh and more

---
+++ webpack.config.dev.js
@@ -0,0 +1,42 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	resolve: {
+	  extensions: [".js", ".jsx", ".ts", ".tsx"],
+	},
+	module: {
+	  rules: [
+		{
+		  test: [/\.jsx?$/, /\.tsx?$/],
+		  use: ["babel-loader"],
+		  exclude: /node_modules/,
+		},
+		{
+		  test: /\.css$/,
+		  use: ["style-loader", "css-loader"],
+		},
+		{
+		  test: /\.(scss|sass)$/,
+		  use: ["style-loader", "css-loader", "sass-loader"],
+		},
+		{
+		  test: /\.(jpe?g|png|gif|svg)$/i,
+		  use: [
+			"file-loader?hash=sha512&digest=hex&name=img/[contenthash].[ext]",
+			"image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false",
+		  ],
+		},
+	  ],
+	},
+	plugins: [new HtmlWebpackPlugin({ template: resolve(__dirname, "./src/index.html") })],
+	externals: {
+	  react: "React",
+	  "react-dom": "ReactDOM",
+	},
+	performance: {
+	  hints: false,
+	},
+  };
+  
